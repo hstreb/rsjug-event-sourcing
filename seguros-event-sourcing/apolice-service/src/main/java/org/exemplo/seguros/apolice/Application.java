345cebc4bd4528cba17c210e64b0dbfac7abaa85
Humberto Streb <hstreb@gmail.com>
2018-04-29 14:28:03 -0300

    Adição de módulos para o projeto de micro-serviços

--- seguros-event-sourcing/apolice-service/src/main/java/org/exemplo/seguros/apolice/Application.java
+++ seguros-event-sourcing/apolice-service/src/main/java/org/exemplo/seguros/apolice/Application.java
@@ -1,4 +1,4 @@
-package org.exemplo.seguros;
+package org.exemplo.seguros.apolice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
